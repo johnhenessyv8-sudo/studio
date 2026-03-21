3c6186ef1e4eab71e6bf17c590ce2b436ba597bb
John Henessy A. Villanueva <johnhenessy.villanueva@neu.edu.ph>
2026-03-21 08:19:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-796625890-3df8a",
+  "appId": "1:309324061903:web:675a09f9b2292f5a64ebba",
+  "apiKey": "AIzaSyDs-fXovODZPocxOrihlf0k0WZj_0JMaIs",
+  "authDomain": "studio-796625890-3df8a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "309324061903"
+};
